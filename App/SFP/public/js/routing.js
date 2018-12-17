a6c01c1122a47cb0fa4a01d898e14cf42b9a72ad
LE Nhu Hien <nhuhien12895@gmail.com>
2018-12-17 14:35:52 +0100

    remove unused files

--- App/SFP/public/js/routing.js
+++ App/SFP/public/js/routing.js
@@ -5,61 +5,16 @@ videoApp.config(function ($stateProvider,$urlRouterProvider) {
         templateUrl: "view/home/home.html",
         controller: "homeCtrl",
     };
-    var loginState = {
-        name: "login",
-        url: "/login",
-        templateUrl: "view/login/login.html",
-        controller: "loginCtrl",
-    };
-    var signinState = {
-        name: "signup",
-        url: "/signup",
-        templateUrl: "view/signup/signup.html",
-        controller: "signupCtrl",
-    };
+   
     var searchPageState = {
         name: "home.searchPage",
         url: "home/search",
         templateUrl: "view/search/search.html",
         controller: "searchCtrl",
     };
-    var videoPlayerState = {
-        name :"home.videoPlayer",
-        url: "home/player",
-        templateUrl: "view/search/player.html",
-        controller: "videoPlayerCtrl",
-    }
-    var historyPageState = {
-        name: "home.historyPage",
-        url: "home/history",
-        templateUrl: "view/search/history.html",
-        controller: "historyCtrl",
-    };
-    var adminPageState =  {
-        name: "admin",
-        url: "/admin",
-        templateUrl: "view/admin/admin-home.html",
-        controller: "adminCtrl",
-    }
-
+ 
     $stateProvider.state(homeState);
-    $stateProvider.state(loginState);
-    $stateProvider.state(signinState);
+
     $stateProvider.state(searchPageState);
-    $stateProvider.state(videoPlayerState);
-    $stateProvider.state(historyPageState);
-    $stateProvider.state(adminPageState);
     $urlRouterProvider.otherwise("/");
 })
-
-angular.module('videoApp').run(['$cookies', '$location','loginService', function ($cookies,$location,loginService) {
-
-    // var token = $cookies.get('token');
-    // console.log(token);
-    // loginService.verify(token,function(res){
-    //     if (!res.data.success) {
-    //         $location.path('/login');
-    //     }
-    // })
-
-  }]);
--- App/SFP/server.js
+++ App/SFP/server.js
@@ -1,7 +1,4 @@
 var path = require('path');
-const axios = require("axios");
-var jwt = require('jsonwebtoken');
-const yml = require("js-yaml");
 const https = require("https"),
   fs = require("fs");
 
@@ -12,7 +9,6 @@ const options = {
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var uuidv4 = require('uuid/v4');
 const request = require("request")
 var app = express();
 app.use(bodyParser.json());
@@ -21,155 +17,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-// const silosConfigPath = "silos.config.yml";
-// try {
-// 	silosConfigTry = yml.safeLoad(fs.readFileSync(path.join(__dirname, silosConfigPath), "utf8"));
-// 	console.log("Silos config file loaded.");
-// } catch (e) {
-// 	console.log("Could not load the silos config file");
-// 	process.exit(1);
-// }
-// var silosConfig = silosConfigTry.silo;
-
-app.post(`/user/register`, (req, res) => {
-  axios.post('http://localhost:8091/register', {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(function (response) {
-    res.json(response.data);
-  }).catch(function (error) {
-    res.send(false);
-  });
-});
-
-// const RSA_PRIVATE_KEY = fs.readFileSync('./config/private.pem');
-
-app.post(`/user/login`, (req, res) => {
-  axios.post('http://localhost:8091/login', {
-    email: req.body.email,
-    password: req.body.password
-  }).then(function (response) {
-    res.json(response.data);
-  }).catch(function (error) {
-    res.send(false);
-  });
-});
-
-app.post(`/user/verify`, (req, res) => {
-  axios.post('http://localhost:8091/verify', {
-    token: req.body.token
-  }).then(function (response) {
-    res.json(response.data);
-  }).catch(function (error) {
-    res.send(false);
-  });
-});
-app.post('/user/adminVerify', function (req, res) {
-  axios.post('http://localhost:8091/adminVerify').then(function (res) {
-    res.json(response.data)
-  }).catch(function (err) {
-    res.send(false)
-  })
-})
-
-app.post('/videos/search', (req, res) => {
-  var site = req.body.site
-  console.log(site);
-
-  if (site.toLowerCase() == "youtube") {
-    console.log('true');
-
-    axios.post('http://localhost:8092/videos/youtube/search', {
-      input: req.body.input
-    }).then(function (response) {
-      res.json(response.data);
-    }).catch(function (error) {
-      res.send(false);
-    });
-  } else if (site.toLowerCase() == "vimeo") {
-    axios.post('http://localhost:8092/videos/vimeo/search', {
-      input: req.body.input
-    }).then(function (response) {
-      res.json(response.data);
-    }).catch(function (error) {
-      res.send(false);
-    });
-  }
-
-});
-
-
-app.get('/video/:site/:videoId', (req, res) => {
-  console.log('server', req.params.videoId);
-  var site = req.params.site
-  var videoId = req.params.videoId;
-  if (site == "youtube")
-    request.get('http://localhost:8092/video/youtube/' + videoId).pipe(res)
-  else if (site == "vimeo")
-    request.get('http://localhost:8092/video/vimeo/' + videoId).pipe(res)
-});
-
-
-
-app.post('/video/getVideoInfo', (req, res) => {
-  var site = req.body.site
-  if (site == "youtube") {
-    axios.post('http://localhost:8092/video/youtube/getVideoInfo', {
-      videoId: req.body.videoId
-    }).then(function (response) {
-      console.log(response.data);
-
-      res.send(response.data);
-    }).catch(function (error) {
-      res.send(false);
-    });
-  } else if (site == "vimeo") {
-    axios.post('http://localhost:8092/video/vimeo/getVideoInfo', {
-      videoId: req.body.videoId
-    }).then(function (response) {
-      console.log(response.data);
-
-      res.send(response.data);
-    }).catch(function (error) {
-      res.send(false);
-    });
-  }
-});
-app.post(`/video/add`, (req, res) => {
-  axios.post('http://localhost:8092/video/add', {
-    name : req.body.name,
-    video_id : req.body.video_id,
-    thumbnailUrl: req.body.thumbnailUrl,
-    description: req.body.description,
-    site :req.body.site
-}).then(function (response) {
-    console.log("add video");
-    res.send(response.data);
-  }).catch(function (error) {
-    res.send(false);
-  });
-});
-app.post(`/video/history`, (req, res) => {
-  axios.post('http://localhost:8092/video/history', {
-    user_id:req.body.user_id,
-    video_id : req.body.video_id,
-}).then(function (response) {
-    res.send(response.data);
-  }).catch(function (error) {
-    res.send(false);
-  });
-});
-
-app.get(`/history/:user_id`, (req, res) => {
-  var user_id = req.params.user_id 
-  axios.get('http://localhost:8092/history/'+user_id).then(function (response) {
-    res.send(response.data);
-  }).catch(function (error) {
-    res.send(false);
-  });
-});
 
 
 app.post('/search', (req, res)=>{
@@ -182,17 +29,7 @@ app.post('/search', (req, res)=>{
     if (response.statusCode == 200) {
       isOk = true
       var items = JSON.parse(response.body).response.docs
-      // for (var i in items) {
-      //   videos.push({
-      //     videoId: items[i].id.videoId,
-      //     site: 'youtube',
-      //     name: items[i].snippet.title,
-      //     embedUrl: "https://www.youtube.com/embed/",
-      //     watchUrl: "https://www.youtube.com//watch?v=",
-      //     thumbnailUrl: items[i].snippet.thumbnails.default.url,
-      //     description: items[i].snippet.description
-      //   })
-      // }
+     
       console.log(items);
       res.send({
         data: items,
